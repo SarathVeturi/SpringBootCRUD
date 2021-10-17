b6716307efa2045e05894da1cd6ff1689ea7668d
SarathVeturi <sarathveturi99@gmail.com>
2021-10-18 00:02:07 +0530

    Added logging - slf4j & logback.xml

--- src/main/java/com/app/spb/controller/ProductPurchaseController.java
+++ src/main/java/com/app/spb/controller/ProductPurchaseController.java
@@ -1,5 +1,7 @@
 package com.app.spb.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,17 +9,35 @@ import org.springframework.web.bind.annotation.RestController;
 import com.app.spb.dto.PurchaseAckResponse;
 import com.app.spb.dto.PurchaseRequest;
 import com.app.spb.service.ProductService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class ProductPurchaseController {
-	
+
 	private final ProductService productService;
+	private final ObjectMapper objectMapper;
 	
+	private final Logger log = LoggerFactory.getLogger(ProductService.class);
+
 	@PostMapping("/buyProduct")
 	public PurchaseAckResponse getResponse(@RequestBody PurchaseRequest request) {
-		return productService.getPurchaseDetails(request);
+
+		try {
+			log.info("Request to API - {}", objectMapper.writeValueAsString(request));
+		} catch (JsonProcessingException ex) {
+			log.error("Error occured - {}", ex.getMessage());
+		}
+
+		PurchaseAckResponse response = productService.getPurchaseDetails(request);
+		try {
+			log.info("Response from API - {}", objectMapper.writeValueAsString(response));
+		} catch (JsonProcessingException ex) {
+			log.error("Error occured - {}", ex.getMessage());
+		}
+		return response;
 	}
 }
